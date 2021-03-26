2780411a17dc6eb8b4e31ec2caa2f960dcb1c482
Ali Esma <824194@stma.k12.mn.us>
2021-03-26 10:22:36 -0500

    If Else Statement

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def addTwoNumbers(x, y):
-    return x + y
+light = input("Stoplight Color:\n")
 
-print(addTwoNumbers(77, 9))
+if light == "green":
+    print("Go")
 
+elif light == "yellow":
+    print("slow")
+
+elif light == "blue":
+    print("CALL 911")
+
+else:
+    print("stop")
